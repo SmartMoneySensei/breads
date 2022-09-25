e896b70d020663edb972f261d55e90f1e05930e9
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-25 12:38:39 -0700

    Instance Route

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,11 +32,13 @@ breads.get('/:id/edit', (req, res) => {
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
-    .then(foundBread => {
-      res.render('show', {
-        bread: foundBread
+      .then(foundBread => {
+        const bakedBy = foundBread.getBakedBy() 
+        console.log(bakedBy)
+        res.render('show', {
+            bread: foundBread
+        })
       })
-    })
     .catch(err => {
       res.render('404')
     })
