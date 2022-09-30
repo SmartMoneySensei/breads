ab79d07f3f4045a63f1260a6ebdcc0bcc9a427c3
Kasean Cobbs-Walton <smartmoneysensei@gmail.com>
2022-09-29 19:44:12 -0700

    async + await

--- views/index.jsx
+++ views/index.jsx
@@ -1,10 +1,10 @@
 const React = require('react')
-const Default = require('./layouts/Default')
+const Default = require('./layouts/default')
 
 function Index ({breads, bakers})  {
     return (
 <Default>
-  <h2>Index Page</h2>
+  <h2 className="text-center">Index Page</h2>
   <h3> Bakers </h3>
   <ul>
     {
